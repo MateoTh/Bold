ab35c2b2287bd5b4a1b347f1132b6cb733d38c5b
MateoTh <mateo.thiebault34@orange.fr>
2023-11-22 14:21:06 +0100

    android code version & rule mistake

--- lib/ui/Rules/step_4.dart
+++ lib/ui/Rules/step_4.dart
@@ -24,7 +24,7 @@ class Step4 extends StatelessWidget {
               "*In his turn, each player can do one of the following:"),
           rulesText(context, "Add or Bet"),
           rulesText(context,
-              "*If he decides to *bet*, the player *takes back his phone*, pulls up his card to get back to his hand and chooses which of his remaining cards he will place *on top of the first one."),
+              "*If he decides to *add*, the player *takes back his phone*, pulls up his card to get back to his hand and chooses which of his remaining cards he will place *on top of the first one."),
           rulesText(context,
               "*After choosing a new card, he pulls them down again *to turn them over*, and places the phone back on the table for all to see."),
           rulesText(context, "*Now it's the turn of the next player."),
