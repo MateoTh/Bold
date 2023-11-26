022847d538b59afd64cbf56ad20db31b183804a4
MateoTh <mateo.thiebault34@orange.fr>
2023-11-26 21:06:41 +0100

    fix gestureDetector detection

--- lib/ui/Shared Widgets/menu_button.dart
+++ lib/ui/Shared Widgets/menu_button.dart
@@ -11,6 +11,7 @@ class MenuButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      behavior: HitTestBehavior.translucent,
       onTap: () => ontap(),
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 4),
